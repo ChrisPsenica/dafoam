86703d30abba6e0521cccf984b6062226b91e27d
Chris Psenica <cpsenica@iastate.edu>
2026-01-17 16:29:24 -0600

    update with new test

--- tests/runRegTests_AeroOptSBO.py
+++ tests/runRegTests_AeroOptSBO.py
@@ -14,6 +14,7 @@ from dafoam.mphys import DAFoamBuilder, OptFuncs
 from mphys.scenario_aerodynamic import ScenarioAerodynamic
 from pygeo.mphys import OM_DVGEOCOMP
 from testFuncs import *
+from pygeo import geo_utils
 from dafoam.pyDAFoam import surrogateOptimization
 
 gcomm = MPI.COMM_WORLD
@@ -26,27 +27,27 @@ if gcomm.rank == 0:
     os.system("cp -r constant/turbulenceProperties.sa constant/turbulenceProperties")
     # replace_text_in_file("system/fvSchemes", "meshWave;", "meshWaveFrozen;")
 
+# aero setup
 U0 = 10.0
 p0 = 0.0
-nuTilda0 = 4.5e-5
-CL_target = 0.5
-aoa0 = 5.13918623195176
 A0 = 0.1
-# rho is used for normalizing CD and CL
-rho0 = 1.0
+twist0 = 3.25
+LRef = 1.0
+nuTilda0 = 4.5e-5
 
-# Input parameters for DAFoam
 daOptions = {
     "designSurfaces": ["wing"],
     "solverName": "DASimpleFoam",
-    "primalMinResTol": 1.0e-8,
-    "primalMinResTolDiff" : 1e8,
-
+    "primalMinResTol": 1.0e-10,
+    "primalMinResTolDiff": 1e4,
+    "writeDeformedFFDs": True,
+    "writeDeformedConstraints": True,
     "primalBC": {
         "U0": {"variable": "U", "patches": ["inout"], "value": [U0, 0.0, 0.0]},
         "p0": {"variable": "p", "patches": ["inout"], "value": [p0]},
         "nuTilda0": {"variable": "nuTilda", "patches": ["inout"], "value": [nuTilda0]},
         "useWallFunction": True,
+        "transport:nu": 1.5e-5,
     },
     "function": {
         "CD": {
@@ -55,7 +56,7 @@ daOptions = {
             "patches": ["wing"],
             "directionMode": "parallelToFlow",
             "patchVelocityInputName": "patchV",
-            "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+            "scale": 1.0 / (0.5 * U0 * U0 * A0),
         },
         "CL": {
             "type": "force",
@@ -63,16 +64,25 @@ daOptions = {
             "patches": ["wing"],
             "directionMode": "normalToFlow",
             "patchVelocityInputName": "patchV",
-            "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+            "scale": 1.0 / (0.5 * U0 * U0 * A0),
+        },
+        "skewness": {
+            "type": "meshQualityKS",
+            "source": "allCells",
+            "coeffKS": 10.0,
+            "metric": "faceSkewness",
+            "scale": 1.0,
+        },
+        "nonOrtho": {
+            "type": "meshQualityKS",
+            "source": "allCells",
+            "coeffKS": 1.0,
+            "metric": "nonOrthoAngle",
+            "scale": 1.0,
         },
     },
-    "adjEqnOption": {"gmresRelTol": 1.0e-6, "pcFillLevel": 1, "jacMatReOrdering": "rcm"},
-    "normalizeStates": {
-        "U": U0,
-        "p": U0 * U0 / 2.0,
-        "nuTilda": nuTilda0 * 10.0,
-        "phi": 1.0,
-    },
+    "adjEqnOption": {"gmresRelTol": 1.0e-10, "pcFillLevel": 1, "jacMatReOrdering": "rcm"},
+    "normalizeStates": {"U": U0, "p": U0 * U0 / 2.0, "phi": 1.0, "nuTilda": 1e-3},
     "inputInfo": {
         "aero_vol_coords": {"type": "volCoord", "components": ["solver", "function"]},
         "patchV": {
@@ -85,7 +95,6 @@ daOptions = {
     },
 }
 
-# Mesh deformation setup
 meshOptions = {
     "gridFile": os.getcwd(),
     "fileType": "OpenFOAM",
@@ -94,118 +103,107 @@ meshOptions = {
 }
 
 
-# Top class to setup the optimization problem
 class Top(Multipoint):
     def setup(self):
-
-        # create the builder to initialize the DASolvers
         dafoam_builder = DAFoamBuilder(daOptions, meshOptions, scenario="aerodynamic")
         dafoam_builder.initialize(self.comm)
 
-        # add the design variable component to keep the top level design variables
+        ################################################################################
+        # MPHY setup
+        ################################################################################
+
+        # ivc to keep the top level DVs
         self.add_subsystem("dvs", om.IndepVarComp(), promotes=["*"])
 
-        # add the mesh component
+        # create the mesh and cruise scenario because we only have one analysis point
         self.add_subsystem("mesh", dafoam_builder.get_mesh_coordinate_subsystem())
 
-        # add the geometry component (FFD)
+        # add the geometry component, we dont need a builder because we do it here.
         self.add_subsystem("geometry", OM_DVGEOCOMP(file="FFD/wingFFD.xyz", type="ffd"))
 
-        # add a scenario (flow condition) for optimization, we pass the builder
-        # to the scenario to actually run the flow and adjoint
-        self.mphys_add_scenario("scenario1", ScenarioAerodynamic(aero_builder=dafoam_builder))
+        self.mphys_add_scenario("cruise", ScenarioAerodynamic(aero_builder=dafoam_builder))
 
-        # need to manually connect the x_aero0 between the mesh and geometry components
-        # here x_aero0 means the surface coordinates of structurally undeformed mesh
         self.connect("mesh.x_aero0", "geometry.x_aero_in")
-        # need to manually connect the x_aero0 between the geometry component and the scenario1
-        # scenario group
-        self.connect("geometry.x_aero0", "scenario1.x_aero")
+        self.connect("geometry.x_aero0", "cruise.x_aero")
+
+        self.add_subsystem("LoD", om.ExecComp("val=CL/CD"))
 
     def configure(self):
 
-        # get the surface coordinates from the mesh component
+        # create geometric DV setup
         points = self.mesh.mphys_get_surface_mesh()
 
-        # add pointset to the geometry component
+        # add pointset
         self.geometry.nom_add_discipline_coords("aero", points)
 
-        # set the triangular points to the geometry component for geometric constraints
+        # create constraint DV setup
         tri_points = self.mesh.mphys_get_triangulated_surface()
         self.geometry.nom_setConstraintSurface(tri_points)
 
-        # use the shape function to define shape variables for 2D airfoil
-        pts = self.geometry.DVGeo.getLocalIndex(0)
-        dir_y = np.array([0.0, 1.0, 0.0])
-        shapes = []
-        for i in range(1, pts.shape[0] - 1):
-            for j in range(pts.shape[1]):
-                # k=0 and k=1 move together to ensure symmetry
-                shapes.append({pts[i, j, 0]: dir_y, pts[i, j, 1]: dir_y})
-        # LE/TE shape, the j=0 and j=1 move in opposite directions so that
-        # the LE/TE are fixed
-        for i in [0, pts.shape[0] - 1]:
-            shapes.append({pts[i, 0, 0]: dir_y, pts[i, 0, 1]: dir_y, pts[i, 1, 0]: -dir_y, pts[i, 1, 1]: -dir_y})
-        self.geometry.nom_addShapeFunctionDV(dvName="shape", shapes=shapes)
+        # add the dv_geo object to the builder solver. This will be used to write deformed FFDs
+        self.cruise.coupling.solver.add_dvgeo(self.geometry.DVGeo)
 
-        # setup the volume and thickness constraints
-        leList = [[1e-4, 0.0, 1e-4], [1e-4, 0.0, 0.1 - 1e-4]]
-        teList = [[0.998 - 1e-4, 0.0, 1e-4], [0.998 - 1e-4, 0.0, 0.1 - 1e-4]]
-        self.geometry.nom_addThicknessConstraints2D("thickcon", leList, teList, nSpan=2, nChord=10)
-        self.geometry.nom_addVolumeConstraint("volcon", leList, teList, nSpan=2, nChord=10)
-        self.geometry.nom_addLERadiusConstraints("rcon", leList, 2, [0.0, 1.0, 0.0], [-1.0, 0.0, 0.0])
-        # NOTE: we no longer need to define the sym and LE/TE constraints
-        # because these constraints are defined in the above shape function
+        # add the dv_con object to the builder solver. This will be used to write deformed constraints
+        self.cruise.coupling.solver.add_dvcon(self.geometry.DVCon)
+
+        # geometry setup
+
+        # Select all points
+        pts = self.geometry.DVGeo.getLocalIndex(0)
+        indexList = pts[:, :, :].flatten()
+        PS = geo_utils.PointSelect("list", indexList)
+        nShapes = self.geometry.nom_addLocalDV(dvName="shape", pointSelect=PS)
 
         # add the design variables to the dvs component's output
-        self.dvs.add_output("shape", val=np.array([0] * len(shapes)))
-        self.dvs.add_output("patchV", val=np.array([U0, aoa0]))
-        # manually connect the dvs output to the geometry and scenario1
-        self.connect("patchV", "scenario1.patchV")
+        self.dvs.add_output("shape", val=np.zeros(nShapes))
+        self.dvs.add_output("patchV", val=np.array([10.0, 3.0]))
+        # manually connect the dvs output to the geometry and cruise
         self.connect("shape", "geometry.shape")
+        self.connect("patchV", "cruise.patchV")
 
         # define the design variables to the top level
-        self.add_design_var("shape", lower=-1.0, upper=1.0, scaler=10.0)
-        # here we fix the U0 magnitude and allows the aoa to change
-        self.add_design_var("patchV", lower=[U0, 0.0], upper=[U0, 10.0], scaler=0.1)
+        self.add_design_var("shape", lower=-1.0, upper=1.0, scaler=0.01)
+        self.add_design_var("patchV", lower=-50.0, upper=50.0, scaler=1.0, indices=[1])
 
-        # add objective and constraints to the top level
-        self.add_objective("scenario1.aero_post.CD", scaler=1.0)
-        self.add_constraint("scenario1.aero_post.CL", equals=CL_target, scaler=1.0)
-        self.add_constraint("geometry.thickcon", lower=0.5, upper=3.0, scaler=1.0)
-        self.add_constraint("geometry.volcon", lower=1.0, scaler=1.0)
-        self.add_constraint("geometry.rcon", lower=0.8, scaler=1.0)
+        # add constraints and the objective
+        self.connect("cruise.aero_post.CD", "LoD.CD")
+        self.connect("cruise.aero_post.CL", "LoD.CL")
+        self.add_objective("LoD.val", scaler=1.0)
+
 
 # openmdao setup
 prob = om.Problem()
 prob.model = Top()
-prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
-om.n2(prob, show_browser=False, outfile="mphys_aero.html")
+prob.setup(mode="rev")
 
 # surrogate optimization setup
-xlimits = np.array([[-0.05, 0.05]] * 10)        
-xlimits[-1] = [0 , 3]                           
-xlimits[-2] = [10 , 10 + 1e-9]                  
-xlimits[-3] = [-0.01 , 0.01]                    
-xlimits[-4] = [-0.01 , 0.01]
+xlimits = np.array([[0, 0.1]] * 22)  # DV bounds
+xlimits[-1] = [3.0, 3.3]  # adjust DV bounds
+xlimits[-2] = [10, 10 + 1e-9]
+xlimits[0:4] = [0.0, 0.0 + 1e-9]
+xlimits[4:8] = [0.03, 0.04]
+xlimits[8:10] = [0.001, 0.002]
+xlimits[10:12] = [0.004, 0.005]
+xlimits[12:16] = [-0.016, 0.0]
+xlimits[16:20] = [0.0, 0.0 + 1e-9]
 
 surrogateOptions = {
-    "optType"    : "constrained",               
-    "criterion"  : "EI",                        
-    "iters"      : 12,                          
-    "numDOE"     : 21,                          
-    "seed"       : 45,                          
-    "dvNames"    : ["shape" , "patchV"],        
-    "dvSizes"    : [8 , 2],                     
-    "dvBounds"   : xlimits,                     
-    "objFunc"    : 'scenario1.aero_post.CD',    
-    "cons"       : ['scenario1.aero_post.CL'],  
-    "conWeights" : [10],                        
-    "consEqs"    : ["x - 0.3"],                 
+    "optType": "constrained",  # type of optimization problem (constrained or unconstrained)
+    "criterion": "EI",  # criterion for next evaluation point determination -> EGO algorithm
+    "iters": 5,  # num iterations to optimize function
+    "numDOE": 12,  # number of sampling points
+    "seed": 41,  # seed value to reproduce results
+    "dvNames": ["shape", "patchV"],  # names of design variables
+    "dvSizes": [20, 2],  # number of points for each design variable
+    "dvBounds": xlimits,  # design variable bounds
+    "objFunc": "LoD.val",  # objective function
+    "maxObj": True,  # maximize lift over drag
+    "cons": ["cruise.aero_post.CL"],  # quantity to constrain
+    "conWeights": [1e8],  # constraint weight
+    "consEqs": ["x - 0.3"],  # constraint equation(s)
 }
 
-surrogateOptimization(surrogateOptions , prob)
-
+surrogateOptimization(surrogateOptions, prob)
 
 if gcomm.rank == 0:
     funcDict = {}
